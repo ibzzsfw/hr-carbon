639737d6e4d63f207e5d112528124d49bcf39738
ibzzsfw <tap.pt33@gmail.com>
2022-02-05 17:03:20 +0700

    fist

---
+++ hr-carbon/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.scss';
+import { Content } from 'carbon-components-react';
+import UI from './components/UI';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <UI />
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+        </Content>
+      </>
+    );
+  }
+}
+
+export default App;
